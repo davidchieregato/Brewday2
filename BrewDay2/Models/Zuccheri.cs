da3d2cbfb131ccff28d1d3466d14110c887e79e8
Davide Ditolve <d.ditolve@campus.unimib.it>
2018-01-21 15:58:33 +0100

    Classi base DB

---
+++ BrewDay2/Models/Zuccheri.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace BrewDay.Models
+{
+    [Table("Zuccheri")]
+    public class Zuccheri
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
+        public int ZuccheriId { get; set; }
+        public String Nome { get; set; }
+        public String Descrizione { get; set; }
+        public String Produttore { get; set; }
+        public Decimal Prezzo { get; set; }
+    }
+}
