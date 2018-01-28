19bd6845caefcadceb99c060c994cdeeea4eb725
Luca De Filippi <l.defilippi1@campus.unimib.it>
2018-01-28 23:22:37 +0100

    add magazzinocontrollertest

---
+++ BrewDay2.Tests/MagazzinoControllerTests.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BrewDay2.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrewDay2.Controllers.Tests
+{
+    [TestClass()]
+    public class MagazzinoControllerTests
+    {
+        [TestMethod()]
+        public void IndexTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DetailsTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CreateTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CreateTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EditTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EditTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DeleteTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DeleteConfirmedTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void IndexTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DetailsTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CreateTest2()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CreateTest3()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EditTest2()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EditTest3()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DeleteTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DeleteConfirmedTest1()
+        {
+            Assert.Fail();
+        }
+    }
+}
