30feefa79570cf920a78c3db44b5dabde6c2651e
Davide Ditolve <d.ditolve@campus.unimib.it>
2018-01-28 22:32:56 +0100

    aggiunti model n a n

---
+++ BrewDay2/Models/LievitiRicetta.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace BrewDay.Models
+{
+    public class LievitiRicetta
+    {
+        [Key, Column(Order = 0)]
+        public int RicettaId { get; set; }
+        [Key, Column(Order = 1)]
+        public int LievitoId { get; set; }
+        public virtual Ricette Ricetta { get; set; }
+        public virtual Lieviti Lievito { get; set; }
+        public Decimal Quantita { get; set; }
+    }
+}
