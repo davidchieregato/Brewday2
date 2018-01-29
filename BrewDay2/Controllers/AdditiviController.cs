de3ba0b6157c24531ba2805c86a86e2d1eaf9289
Davide Ditolve <d.ditolve@campus.unimib.it>
2018-01-29 23:26:15 +0100

    Additivi index controller

---
+++ BrewDay2/Controllers/AdditiviController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.EnterpriseServices;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BrewDay.Models;
+using BrewDay2.Models;
+
+namespace BrewDay2.Controllers
+{
+    [Authorize]
+    public class AdditiviController : Controller
+    {
+
+        //SE SERVE RECUPERARE UTENTE CORRENTE
+        //var me = db.Users.First(x => x.UserName == User.Identity.Name);
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Additivi
+        public ActionResult Index()
+        {
+            return View(db.Additivi.ToList());
+        }
+
+     
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
--- BrewDay2/Models/IdentityModels.cs
+++ BrewDay2/Models/IdentityModels.cs
@@ -4,7 +4,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 
-namespace BrewDay2.Models
+namespace BrewDay.Models
 {
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
@@ -25,9 +25,26 @@ namespace BrewDay2.Models
         {
         }
 
+        public DbSet<Additivi> Additivi { get; set; }
+        public DbSet<Lieviti> Lieviti { get; set; }
+        public DbSet<Luppoli> Luppoli { get; set; }
+        public DbSet<Malti> Malti { get; set; }
+        public DbSet<Zuccheri> Zuccheri { get; set; }
+        public DbSet<Ricette> Ricette { get; set; }
+        public DbSet<AdditiviRicetta> AdditiviRicettas { get; set; }
+        public DbSet<LievitiRicetta> LievitiRicettas { get; set; }
+        public DbSet<LuppoliRicetta> LuppoliRicettas { get; set; }
+        public DbSet<MaltiRicetta> MaltiRicettas { get; set; }
+        public DbSet<ZuccheriRicetta> ZuccheriRicettas { get; set; }
+        //public DbSet<Magazzino> Magazzini { get; set; }
+
+
         public static ApplicationDbContext Create()
         {
             return new ApplicationDbContext();
         }
+
+        public System.Data.Entity.DbSet<BrewDay.Models.Magazzino> Magazzinoes { get; set; }
     }
+
 }
