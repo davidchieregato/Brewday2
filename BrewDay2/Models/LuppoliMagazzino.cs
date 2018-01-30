8f927e6c2043d22fc93f5cc15cd3d23ca8d3eb78
Luca De Filippi <l.defilippi1@campus.unimib.it>
2018-01-30 16:37:40 +0100

    add model LuppoliMagazzino

---
+++ BrewDay2/Models/LuppoliMagazzino.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace BrewDay2.Models
+{
+    public class LuppoliMagazzino
+    {
+        [Key, Column(Order = 0)]
+        public int MagazzinoId { get; set; }
+        [Key, Column(Order = 1)]
+        public int LuppoliId { get; set; }
+        public virtual Magazzino Magazzino { get; set; }
+        public virtual Luppoli Luppolo { get; set; }
+        public Decimal Quantita { get; set; }
+    }
+
+}
