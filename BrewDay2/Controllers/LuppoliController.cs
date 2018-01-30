be7fb063d025c67735c968a3b6e1b42ffa205c3a
Luca De Filippi <l.defilippi1@campus.unimib.it>
2018-01-30 16:00:46 +0100

    add get luppoli/delete

--- BrewDay2/Controllers/LuppoliController.cs
+++ BrewDay2/Controllers/LuppoliController.cs
@@ -90,5 +90,20 @@ namespace BrewDay.Controllers
             return View(luppoli);
         }
 
+        // GET: Luppoli/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Luppoli luppoli = db.Luppoli.Find(id);
+            if (luppoli == null)
+            {
+                return HttpNotFound();
+            }
+            return View(luppoli);
+        }
+
     }
 }
