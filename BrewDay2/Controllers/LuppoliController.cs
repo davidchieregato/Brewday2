0e7a6a42051048dd3a2892348805dc1f707dfb7d
Luca De Filippi <l.defilippi1@campus.unimib.it>
2018-01-30 15:46:07 +0100

    add LuppoliController

---
+++ BrewDay2/Controllers/LuppoliController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BrewDay2.Models;
+
+namespace BrewDay.Controllers
+{
+    [Authorize]
+    public class LuppoliController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Luppoli
+        public ActionResult Index()
+        {
+            return View(db.Luppoli.ToList());
+        }
+
+        // GET: Luppoli/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Luppoli luppoli = db.Luppoli.Find(id);
+            if (luppoli == null)
+            {
+                return HttpNotFound();
+            }
+            return View(luppoli);
+        }
+
+        // GET: Luppoli/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+	}
+}
