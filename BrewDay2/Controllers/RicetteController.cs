6ab14b0d3f5372ddd6a8378ce684fe4364d3c1fe
Davide Ditolve <d.ditolve@campus.unimib.it>
2018-01-31 01:03:07 +0100

    fix import - necessaria ricompilazione

--- BrewDay2/Controllers/RicetteController.cs
+++ BrewDay2/Controllers/RicetteController.cs
@@ -4,9 +4,8 @@ using System.Data;
 using System.Data.Entity;
 using System.Linq;
 using System.Net;
-using System.Web;
 using System.Web.Mvc;
-using BrewDay.Models;
+using BrewDay2.Models;
 
 namespace BrewDay.Controllers
 {
--- BrewDay2/Controllers/ZuccherisController.cs
+++ BrewDay2/Controllers/ZuccherisController.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Data.Entity;
+﻿using System.Data.Entity;
 using System.Linq;
 using System.Net;
-using System.Web;
 using System.Web.Mvc;
-using BrewDay.Models;
+using BrewDay2.Models;
 
 namespace BrewDay.Controllers
 {
