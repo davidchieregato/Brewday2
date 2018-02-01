e334738211dbe9d077f30d614cdb7292a23b2b21
Luca De Filippi <l.defilippi1@campus.unimib.it>
2018-02-01 23:59:54 +0100

    commenti additivicontroller

--- BrewDay2/Controllers/AdditiviController.cs
+++ BrewDay2/Controllers/AdditiviController.cs
@@ -21,6 +21,12 @@ namespace BrewDay2.Controllers
         private readonly ApplicationDbContext db = new ApplicationDbContext();
 
         // GET: Additivi
+        /// <summary>
+        /// Metodo invocato alla richiesta di Additivi
+        /// Setta nella variabile speciale ViewBag l'utente loggato e restituisce 
+        /// alla view  una lista di additivi
+        /// </summary>
+        /// <returns></returns>
         public ActionResult Index()
         {
             ViewBag.me = User.Identity.GetUserId();
@@ -28,6 +34,13 @@ namespace BrewDay2.Controllers
         }
 
         // GET: Additivi/Details/5
+        /// <summary>
+        /// Metodo invocato alla richiesta di dettagli su un additivo
+        /// Restituisce errore se id è nullo o non esiste
+        /// Altrimenti restituisce l'elemento cercato alla pagina
+        /// </summary>
+        /// <param name="id">Valore della chiave per model Additivi</param>
+        /// <returns></returns>
         public ActionResult Details(int? id)
         {
             if (id == null)
@@ -43,6 +56,12 @@ namespace BrewDay2.Controllers
         }
 
         // GET: Additivi/Create
+        /// <summary>
+        /// Metodo invocato alla creazione di un nuovo additivo
+        /// Assegna uno user all'additivo con i campi da compilare nella view
+        /// Ritorna la vista 
+        /// </summary>
+        /// <returns></returns>
         public ActionResult Create()
         {
             Additivi a = new Additivi();
@@ -54,6 +73,12 @@ namespace BrewDay2.Controllers
         // POST: Additivi/Create
         // Per proteggere da attacchi di overposting, abilitare le proprietà a cui eseguire il binding. 
         // Per ulteriori dettagli, vedere https://go.microsoft.com/fwlink/?LinkId=317598.
+        /// <summary>
+        /// Metodo invocato alla submit della form per la creazione di un Additivo nella pagina equivalente
+        /// Se il modello è valido, crea e aggiunge l' Additivo al database
+        /// </summary>
+        /// <param name="additivi">Oggetto da inserire nel database</param>
+        /// <returns></returns>
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Create(Additivi additivi)
@@ -69,6 +94,14 @@ namespace BrewDay2.Controllers
         }
 
         // GET: Additivi/Edit/5
+        /// <summary>
+        /// Metodo invocato alla modifica di un nuovo additivo
+        /// Se l' ID è nullo ritorna uno StatusCode di BadRequest
+        /// Se l' ID non è presente nel database ritorna HttpNotFound
+        /// Se è presente ritorna la vista dell' Additivo trovato
+        /// </summary>
+        /// <param name="id">Valore della chiave per il model Additivi</param>
+        /// <returns></returns>
         public ActionResult Edit(int? id)
         {
             if (id == null)
@@ -85,6 +118,13 @@ namespace BrewDay2.Controllers
         // POST: Additivi/Edit/5
         // Per proteggere da attacchi di overposting, abilitare le proprietà a cui eseguire il binding. 
         // Per ulteriori dettagli, vedere https://go.microsoft.com/fwlink/?LinkId=317598.
+        /// <summary>
+        /// Metodo invocato alla submit della form per la modifica di un Additivo nella pagina equivalente
+        /// Se il modello è valido sostituisce nelle database l' Additivo con quello modificato, salva i cambiamenti
+        /// Ritorna la vista
+        /// </summary>
+        /// <param name="additivi">Oggetto da modificare nel database</param>
+        /// <returns></returns>
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Edit([Bind(Include = "Id,Nome,Descrizione,Produttore,Prezzo")] Additivi additivi)
@@ -99,6 +139,14 @@ namespace BrewDay2.Controllers
         }
 
         // GET: Additivi/Delete/5
+        /// <summary>
+        /// Metodo invocato alla cancellazione di un additivo
+        /// Se l' ID è nullo ritorna uno StatusCode di BadRequest
+        /// Se l' ID non è presente nel database ritorna HttpNotFound
+        /// Se è presente ritorna la vista dell' Additivo trovato
+        /// </summary>
+        /// <param name="id">Valore della chiave per il model Additivi</param>
+        /// <returns></returns>
         public ActionResult Delete(int? id)
         {
             if (id == null)
@@ -113,6 +161,14 @@ namespace BrewDay2.Controllers
             return View(additivi);
         }
 
+        /// <summary>
+        /// <summary>
+        /// Metodo invocato alla submit della form per la cancellazione di un Additivo nella pagina equivalente
+        /// Se il modello è valido rimuove nel database l' Additivo e salva i cambiamenti
+        /// Ritorna la vista
+        /// </summary>
+        /// <param name="id">Valore della chiave dell'additivo da eliminare</param>
+        /// <returns></returns>
         // POST: Additivi/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
