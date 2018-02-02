edb4ce8698da81b93c364a4546c764799be3de6a
Luca De Filippi <l.defilippi1@campus.unimib.it>
2018-02-02 11:35:27 +0100

    updated descriptions

--- BrewDay2/Controllers/ZuccherisController.cs
+++ BrewDay2/Controllers/ZuccherisController.cs
@@ -17,6 +17,12 @@ namespace BrewDay2.Controllers
         private readonly ApplicationDbContext db = new ApplicationDbContext();
 
         // GET: Zuccheris
+        /// <summary>
+        /// Metodo invocato alla richiesta di Zuccheris
+        /// Setta nella variabile speciale ViewBag l'utente loggato e restituisce 
+        /// alla view  una lista di zuccheri
+        /// </summary>
+        /// <returns></returns>
         public ActionResult Index()
         {
             ViewBag.me = User.Identity.GetUserId();
@@ -24,6 +30,13 @@ namespace BrewDay2.Controllers
         }
 
         // GET: Zuccheris/Details/5
+        /// <summary>
+        /// Metodo invocato alla richiesta di dettagli su uno zucchero
+        /// Restituisce errore se id è nullo o non esiste
+        /// Altrimenti restituisce l'elemento cercato alla pagina
+        /// </summary>
+        /// <param name="id">Valore della chiave per model Zuccheris</param>
+        /// <returns></returns>
         public ActionResult Details(int? id)
         {
             if (id == null)
@@ -39,6 +52,12 @@ namespace BrewDay2.Controllers
         }
 
         // GET: Zuccheris/Create
+        /// <summary>
+        /// Metodo invocato alla creazione di un nuovo zucchero
+        /// Assegna uno user lo zucchero con i campi da compilare nella view
+        /// Ritorna la vista 
+        /// </summary>
+        /// <returns></returns>
         public ActionResult Create()
         {
             Zuccheri z = new Zuccheri();
@@ -49,6 +68,12 @@ namespace BrewDay2.Controllers
         // POST: Zuccheris/Create
         // Per proteggere da attacchi di overposting, abilitare le proprietà a cui eseguire il binding. 
         // Per ulteriori dettagli, vedere https://go.microsoft.com/fwlink/?LinkId=317598.
+        /// <summary>
+        /// Metodo invocato alla submit della form per la creazione di uno Zuccheris nella pagina equivalente
+        /// Se il modello è valido, crea e aggiunge lo zucchero al database
+        /// </summary>
+        /// <param name="zuccheri">Oggetto da inserire nel database</param>
+        /// <returns></returns>
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Create([Bind(Include = "Id,Nome,Descrizione,Produttore,Prezzo")] Zuccheri zuccheri)
@@ -64,6 +89,14 @@ namespace BrewDay2.Controllers
         }
 
         // GET: Zuccheris/Edit/5
+        /// <summary>
+        /// Metodo invocato alla modifica di un nuovo zucchero
+        /// Se l' ID è nullo ritorna uno StatusCode di BadRequest
+        /// Se l' ID non è presente nel database ritorna HttpNotFound
+        /// Se è presente ritorna la vista del Zuccheris trovato
+        /// </summary>
+        /// <param name="id">Valore della chiave per il model Zuccheris</param>
+        /// <returns></returns>
         public ActionResult Edit(int? id)
         {
             if (id == null)
@@ -81,6 +114,13 @@ namespace BrewDay2.Controllers
         // POST: Zuccheris/Edit/5
         // Per proteggere da attacchi di overposting, abilitare le proprietà a cui eseguire il binding. 
         // Per ulteriori dettagli, vedere https://go.microsoft.com/fwlink/?LinkId=317598.
+        /// <summary>
+        /// Metodo invocato alla submit della form per la modifica di uno zucchero nella pagina equivalente
+        /// Se il modello è valido sostituisce nelle database lo zucchero con quello modificato, salva i cambiamenti
+        /// Ritorna la vista
+        /// </summary>
+        /// <param name="zuccheri">Oggetto da modificare nel database</param>
+        /// <returns></returns>
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Edit([Bind(Include = "Id,Nome,Descrizione,Produttore,Prezzo")] Zuccheri zuccheri)
@@ -95,6 +135,14 @@ namespace BrewDay2.Controllers
         }
 
         // GET: Zuccheris/Delete/5
+        /// <summary>
+        /// Metodo invocato alla cancellazione di uno zucchero
+        /// Se l' ID è nullo ritorna uno StatusCode di BadRequest
+        /// Se l' ID non è presente nel database ritorna HttpNotFound
+        /// Se è presente ritorna la vista dello zucchero trovato
+        /// </summary>
+        /// <param name="id">Valore della chiave per il model Zuccheris</param>
+        /// <returns></returns>
         public ActionResult Delete(int? id)
         {
             if (id == null)
@@ -110,6 +158,13 @@ namespace BrewDay2.Controllers
         }
 
         // POST: Zuccheris/Delete/5
+        /// <summary>
+        /// Metodo invocato alla submit della form per la cancellazione di uno zucchero nella pagina equivalente
+        /// Se il modello è valido rimuove nel database lo zucchero e salva i cambiamenti
+        /// Ritorna la vista
+        /// </summary>
+        /// <param name="id">Valore della chiave dello zucchero da eliminare</param>
+        /// <returns></returns>
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(int id)
